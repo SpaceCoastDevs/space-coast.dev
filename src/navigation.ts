bd2d4d173ac8bebe45f07edddf9a110cc8fedc14
Gil Creque <gil@codecraftworks.com>
2024-09-08 17:39:25 +0000

    Added footer back

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,3 +1,4 @@
+import smallLogo from '~/assets/favicons/favicon-32x32.png';
 import { getAsset, getBlogPermalink, getPermalink } from './utils/permalinks';
 
 export const headerData = {
@@ -27,65 +28,61 @@ export const headerData = {
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
+  // links: [
+  //   {
+  //     title: 'Product',
+  //     links: [
+  //       { text: 'Features', href: '#' },
+  //       { text: 'Security', href: '#' },
+  //       { text: 'Team', href: '#' },
+  //       { text: 'Enterprise', href: '#' },
+  //       { text: 'Customer stories', href: '#' },
+  //       { text: 'Pricing', href: '#' },
+  //       { text: 'Resources', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Platform',
+  //     links: [
+  //       { text: 'Developer API', href: '#' },
+  //       { text: 'Partners', href: '#' },
+  //       { text: 'Atom', href: '#' },
+  //       { text: 'Electron', href: '#' },
+  //       { text: 'AstroWind Desktop', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Support',
+  //     links: [
+  //       { text: 'Docs', href: '#' },
+  //       { text: 'Community Forum', href: '#' },
+  //       { text: 'Professional Services', href: '#' },
+  //       { text: 'Skills', href: '#' },
+  //       { text: 'Status', href: '#' },
+  //     ],
+  //   },
+  //   {
+  //     title: 'Company',
+  //     links: [
+  //       { text: 'About', href: '#' },
+  //       { text: 'Blog', href: '#' },
+  //       { text: 'Careers', href: '#' },
+  //       { text: 'Press', href: '#' },
+  //       { text: 'Inclusion', href: '#' },
+  //       { text: 'Social Impact', href: '#' },
+  //       { text: 'Shop', href: '#' },
+  //     ],
+  //   },
+  // ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/SpaceCoast_Devs' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/buildandrundev' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/spacecoastdevs' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/SpaceCoastDevs' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src=${smallLogo.src} alt="Space Coast Devs logo" loading="lazy"></img>
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://space-coast.dev/"> Space Coast Devs</a> · All rights reserved.
   `,
 };
