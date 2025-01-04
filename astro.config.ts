a90e3cf4b7cfa2aa764f36790825cbb9a3fc04db
Gil Creque <gil@codecraftworks.com>
2025-01-04 23:20:58 +0000

    January 2025 Events

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  server: { port: 4321, host: true },
   integrations: [
     tailwind({
       applyBaseStyles: false,
